c3879e883b30338dd75953495de5a5059d5cd781
Yadhu121 <ryadhukrishnan0@gmail.com>
2025-03-29 19:54:27 +0530

    set

--- charity/settings.py
+++ charity/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auth_app',
-    'Organization'
+    'Organization',
+    'donations'
 ]
 
 MIDDLEWARE = [
