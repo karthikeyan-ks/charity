faed806356aae5ba1b819bcabbe524cc831afecf
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-03-30 13:22:02 +0530

    change

--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Donations,AvailableDays,Resource
+admin.site.register(Donations)
+admin.site.register(AvailableDays)
+admin.site.register(Resource)
 # Register your models here.
